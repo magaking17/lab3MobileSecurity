13d85ceceef9d8f198a0ae79c347727b46830d67
magaking17 <yeltaev17@gmail.com>
2024-02-18 15:15:23 +0600

    commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Magzhan_Yeltayev_sis2125_lab3"
+include(":app")
+ 
